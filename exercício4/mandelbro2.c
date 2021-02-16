a70329765ec07fc9f1ddc5f82298ef91b769ba75
Ruan Felipe <44856704+pevifol@users.noreply.github.com>
2021-02-16 13:27:17 -0300

    Biblioteca de visual removida, e agora printa o tempo de execução

--- exercício4/mandelbro2.c
+++ exercício4/mandelbro2.c
@@ -21,99 +21,21 @@ typedef struct complextype
 
 void main(int argc, char *argv[])
 {
-	Window		win;                            /* initialization for a window */
-	unsigned
-	int             width, height,                  /* window size */
-                        x, y,                           /* window position */
-                        border_width,                   /*border width in pixels */
-                        display_width, display_height,  /* size of screen */
-                        screen;                         /* which screen */
-
-	char            *window_name = "Mandelbrot Set", *display_name = NULL;
-	GC              gc;
-	unsigned
-	long		valuemask = 0;
-	XGCValues	values;
-	Display		*display;
-	XSizeHints	size_hints;
-	Pixmap		bitmap;
-	XPoint		points[800];
-	FILE		*fp, *fopen ();
-	char		str[100];
-	
-	XSetWindowAttributes attr[1];
-
        /* Mandlebrot variables */
         int i, j, k,rank,numprocs,l;
         Compl	z, c;
         float	lengthsq, temp;
         float* ponto;
         float** pontos;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);    
-	/* connect to Xserver */
-    if ( rank == 0 ) {
-	if (  (display = XOpenDisplay (display_name)) == NULL ) {
-	   fprintf (stderr, "drawon: cannot connect to X server %s\n",
-				XDisplayName (display_name) );
-	exit (-1);
-	}
-	
-	/* get screen size */
-
-	screen = DefaultScreen (display);
-	display_width = DisplayWidth (display, screen);
-	display_height = DisplayHeight (display, screen);
-
-	/* set window size */
-
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+        MPI_Comm_size (MPI_COMM_WORLD, &numprocs);    
 	width = X_RESN;
 	height = Y_RESN;
-
-	/* set window position */
-
-	x = 0;
-	y = 0;
-
-        /* create opaque window */
-
-	border_width = 4;
-	win = XCreateSimpleWindow (display, RootWindow (display, screen),
-				x, y, width, height, border_width, 
-				BlackPixel (display, screen), WhitePixel (display, screen));
-
-	size_hints.flags = USPosition|USSize;
-	size_hints.x = x;
-	size_hints.y = y;
-	size_hints.width = width;
-	size_hints.height = height;
-	size_hints.min_width = 300;
-	size_hints.min_height = 300;
-	
-	XSetNormalHints (display, win, &size_hints);
-	XStoreName(display, win, window_name);
-
-        /* create graphics context */
-
-	gc = XCreateGC (display, win, valuemask, &values);
-
-	XSetBackground (display, gc, WhitePixel (display, screen));
-	XSetForeground (display, gc, DefaultColormap (display, screen));
-	XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
-
-	attr[0].backing_store = Always;
-	attr[0].backing_planes = 1;
-	attr[0].backing_pixel = DefaultColormap(display, screen);
-
-	XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
-
-	XMapWindow (display, win);
-	XSync(display, 0);
-    }
         /* Calculate and draw points */
         ponto = malloc(sizeof(float)*2);
         pontos= malloc(sizeof(float)*2*Y_RESN*(X_RESN/numprocs));
+	t0 = MPI_Wtime()
         l = 0;       
         for(i = rank; i < X_RESN; i = i + numprocs) { //Calculamos faixas vertícais. 
         for(j=0; j < Y_RESN; j++) {
@@ -150,9 +72,6 @@ void main(int argc, char *argv[])
         MPI_Get_count(&stat,MPI_FLOAT,&l);
         pontos = malloc(sizeof(float)*l);
         MPI_Recv(&pontos, l, MPI_FLOAT, i, MPI_ANY_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        for (j = 0; j < l; j++) {
-        XDrawPoint (display, win, gc, pontos[j][1], pontos[j][0]);
-        }
         }
         }
 
@@ -161,9 +80,10 @@ void main(int argc, char *argv[])
         MPI_Isend(&pontos, l, MPI_FLOAT, 0, 111, MPI_COMM_WORLD,&req);      
         }
 	 
-	XFlush (display);
-	sleep (75);
-
+        t1 = MPI_Wtime();
+	if (rank == 0) {
+	printf("Tempo da rodada: %f",t1-t0);
+	}
 	/* Program Finished */
 
 }
